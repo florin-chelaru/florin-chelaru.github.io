649069abc4612be9ebcf8dafadac92793d928e32
Florin Chelaru <florin.chelaru@yahoo.com>
2015-07-24 10:56:10 -0400

    add parallel demo (hidden directory)

---
+++ parallel/src/polyfill.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Florin Chelaru ( florin [dot] chelaru [at] gmail [dot] com )
+ * Date: 7/20/2015
+ * Time: 1:05 PM
+ */
---
+++ parallel/src/test/my-test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Florin Chelaru ( florin [dot] chelaru [at] gmail [dot] com )
+ * Date: 7/20/2015
+ * Time: 9:12 AM
+ */
+
+var parallel = parallel || {};
+
+parallel.florinTest = function(x, y) {
+  console.log(x + " + " + y + " = " + (x+y));
+  return x + y;
+};
